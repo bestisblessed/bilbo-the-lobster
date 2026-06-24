d03ca34a26cb3bc7ed78666d26d9561f219cbefc
bestisblessed <tyler.durette@gmail.com>
2026-06-24 15:59:41 -0400

    im dumb

---
+++ skills/sync-sync-codex-envs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#scp -r donpablo:~/.codex/skills/sync-codex-envs/ /Users/td/Code/bilbo-the-lobster/skills/sync-codex-envs
+cp -r ~/.codex/skills/sync-codex-envs/* /Users/td/Code/bilbo-the-lobster/skills/sync-codex-envs
